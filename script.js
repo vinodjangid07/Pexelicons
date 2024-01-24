dfe452502edb521a3be232edfd0324027da1ede4
Vinod Jangid <infovinodjangid@gmail.com>
2024-01-24 14:33:03 +0530

    Preloader update

--- script.js
+++ script.js
@@ -1,3 +1,29 @@
+//  
+// ██╗░░░██╗██╗███╗░░██╗░█████╗░██████╗░
+// ██║░░░██║██║████╗░██║██╔══██╗██╔══██╗
+// ╚██╗░██╔╝██║██╔██╗██║██║░░██║██║░░██║
+// ░╚████╔╝░██║██║╚████║██║░░██║██║░░██║
+// ░░╚██╔╝░░██║██║░╚███║╚█████╔╝██████╔╝
+// ░░░╚═╝░░░╚═╝╚═╝░░╚══╝░╚════╝░╚═════╝░
+// ░░░░░██╗░█████╗░███╗░░██╗░██████╗░██╗██████╗░
+// ░░░░░██║██╔══██╗████╗░██║██╔════╝░██║██╔══██╗
+// ░░░░░██║███████║██╔██╗██║██║░░██╗░██║██║░░██║
+// ██╗░░██║██╔══██║██║╚████║██║░░╚██╗██║██║░░██║
+// ╚█████╔╝██║░░██║██║░╚███║╚██████╔╝██║██████╔╝
+// ░╚════╝░╚═╝░░╚═╝╚═╝░░╚══╝░╚═════╝░╚═╝╚═════╝░
+
+// preloader script............
+    
+var loader = document.getElementById("preloader");
+window.addEventListener("load", function () {
+    setTimeout(function() {
+        loader.style.display = "none";
+        document.body.classList.add('scroll-enable');
+        }, 1500);
+  
+  
+})
+
 const visualTogglebutton = document.getElementById('visual-toggle-button');
 const checkbox = document.getElementById('visual-toggle');
 
